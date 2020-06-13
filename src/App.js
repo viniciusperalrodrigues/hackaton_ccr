79e45a2b0e9b85a42ffcd77a472d4ab9e4cb4d91
Roberto Nobre <robertonobre90@gmail.com>
2020-06-13 14:00:02 -0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Home from './pages/Home'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-const App = () => <Home />
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Home} />
+    </Switch>
+  </BrowserRouter>
+)
 
 export default App;
